e783caad4f92f72ed1cb8dbb7d33b1664fcd24d1
Json <1103562550@qq.com>
2020-11-13 18:58:52 +0800

    后台管理

---
+++ client/src/pages/order/order.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './order.less'
+
+/*
+Home路由组件
+ */
+export default function Order(props) {
+  return (
+    <div className="order">
+      开发中...
+    </div>
+  )
+}
